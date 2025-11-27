062b45f8680293afad6bb7da167a64fcddcd6c2a
alancontrerasss <alanfernando081004@gmail.com>
2025-11-27 11:16:09 -0600

    Use environment variables for database connection

--- config/conexion.php
+++ config/conexion.php
@@ -1,13 +1,13 @@
 <?php
-// config/conexion.php
 date_default_timezone_set('America/Mexico_City');
 
-$host = "localhost";
-$usuario = "root";
-$clave = "alan2004";
-$bd = "oficina";
+$host   = getenv('MYSQLHOST');
+$usuario = getenv('MYSQLUSER');
+$clave   = getenv('MYSQLPASSWORD');
+$bd      = getenv('MYSQLDATABASE');
+$puerto  = getenv('MYSQLPORT');
 
-$conexion = new mysqli($host, $usuario, $clave, $bd);
+$conexion = new mysqli($host, $usuario, $clave, $bd, $puerto);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
