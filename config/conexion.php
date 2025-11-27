5cb08e12c4b5c24bd1fee74a5c299877d65473ce
alancontrerasss <alanfernando081004@gmail.com>
2025-11-27 11:41:44 -0600

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,17 +1,17 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-$host   = getenv('MYSQLHOST');
+$host    = getenv('MYSQLHOST');
 $usuario = getenv('MYSQLUSER');
 $clave   = getenv('MYSQLPASSWORD');
 $bd      = getenv('MYSQLDATABASE');
 $puerto  = getenv('MYSQLPORT');
 
-$conexion = new mysqli($host, $usuario, $clave, $bd, $puerto);
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+try {
+    $dsn = "mysql:host=$host;port=$puerto;dbname=$bd;charset=utf8mb4";
+    $conexion = new PDO($dsn, $usuario, $clave);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
-
-$conexion->set_charset("utf8mb4");
 ?>
